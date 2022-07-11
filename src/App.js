20c68cf64ed04dd3ed3b14a483690083f2d66473
Dwi Pasca <dwipascanursanti020@gmail.com>
2022-07-12 06:50:38 +0800

    initial project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
 
-function App() {
+import { Container, Typography, Box, Button } from "@mui/material";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fixed>
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App example
+        </Typography>
+        <Button variant="contained">Hello World</Button>
+        <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }} />
+      </Box>
+    </Container>
   );
 }
-
-export default App;
